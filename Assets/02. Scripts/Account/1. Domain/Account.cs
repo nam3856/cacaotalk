3d291c0536d6b4403082b064fc9ff1ed618f99f5
nam3856 <nam3856@gmail.com>
2025-06-19 17:12:13 +0900

    feature: Comment

--- Assets/02. Scripts/Account/1. Domain/Account.cs
+++ Assets/02. Scripts/Account/1. Domain/Account.cs
@@ -23,7 +23,20 @@ public class Account
         ImageIndex = UnityEngine.Random.Range(0, 5); // 0~4 랜덤
     }
 
+    public Account(string email, string nickname, int imageIndex)
+    {
+        var emailSpec = new AccountEmailSpecification();
+        if (!emailSpec.IsSatisfiedBy(email))
+            throw new System.Exception(emailSpec.ErrorMessage);
 
+        var nickSpec = new AccountNicknameSpecification();
+        if (!nickSpec.IsSatisfiedBy(nickname))
+            throw new System.Exception(nickSpec.ErrorMessage);
+
+        Email = email;
+        Nickname = nickname;
+        ImageIndex = Mathf.Clamp(imageIndex, 0, 4); // 안정성
+    }
     public AccountDTO ToDTO()
     {
         return new AccountDTO(Email, Nickname, ImageIndex);
